7cab87e8dfd9c58306a7921880e718e3279533d9
sunny14 kk <faina.private@gmail.com>
2024-01-09 19:21:23 +0200

    username creation

--- CreateUserApp/Program.cs
+++ CreateUserApp/Program.cs
@@ -2,6 +2,27 @@
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("Enter your full name in small letters:");
+        String fullname = Console.ReadLine();
 
+        //treat a middle name case
+
+        String[] NameParts = fullname.Split(' ');
+
+        String username;
+
+        String Name = NameParts[0];
+        String Lastname = NameParts[NameParts.Length - 1];
+
+        if (Lastname.Length >= 3)
+        {
+            username = Name + Lastname.Substring(0, 3);
+        }
+        else
+        {
+            username = Name + Lastname;
+        }
+
+        Console.WriteLine("Your username is: " + username);
     }
 }
